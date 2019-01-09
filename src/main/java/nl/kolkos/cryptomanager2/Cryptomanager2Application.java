1a1b30762082a11157234c0fc6cc3b3357b47476
anton <anton@antergos>
2019-01-09 17:25:25 +0100

    empty project

---
+++ src/main/java/nl/kolkos/cryptomanager2/Cryptomanager2Application.java
@@ -0,0 +1,14 @@
+package nl.kolkos.cryptomanager2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cryptomanager2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cryptomanager2Application.class, args);
+	}
+
+}
+
